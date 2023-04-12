503251f4ab2a29de021d9a95a72248656f9f8509
Kevin <klums24@gmail.com>
2023-04-12 10:51:20 -0700

    complete lab phase 1

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function addingEventListener() {
     const input = document.getElementById('button');
 
+
+
+input.addEventListener('click', clickAlert);
+}
+
 function clickAlert() {
   alert('I was clicked!');
 }
 
-input.addEventListener('click', clickAlert);
-}
+addingEventListener();
